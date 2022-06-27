51f454730f53dc806104d7d6d144282aab79e286
anujy <anuj.y@hotmail.com>
2022-06-27 15:17:39 -0400

    working sketchpad with slider, 1-100px

--- script.js
+++ script.js
@@ -1,34 +1,50 @@
 const gridContainer = document.getElementsByClassName('grid-container')[0];
 
-function makeGrid(size) {
+function clearGrid(element) {
+    while(element.firstChild) {
+        element.removeChild(element.firstChild);
+    }
+}
+
+function makeGrid(size) { //generate size x size grid;
+    clearGrid(gridContainer);
     gridContainer.style.setProperty('--grid-rows', size);
     gridContainer.style.setProperty('--grid-columns', size);
     for (let n=0; n<(size**2); n++) {
         let cell = document.createElement('div');
-        gridContainer.appendChild(cell).className = 'grid-cell';
+        cell.className = 'grid-cell';
+        sketchableCell(cell);
+        gridContainer.appendChild(cell);
     }
 }
 
-makeGrid(16, 16);
-
-const cells = Array.from(document.getElementsByClassName('grid-cell'));
-
 let mouseDown = false;
+document.addEventListener('mouseup', () => {mouseDown=false});
 
-for (let cell of cells) {
+function sketchableCell(e) {
 
-    cell.addEventListener('mousedown', () => {
+    e.addEventListener('mousedown', () => {
         mouseDown=true;
-        cell.classList.add('colored-cell');
+        e.classList.add('colored-cell');
     });
-    document.addEventListener('mouseup', () => {mouseDown=false});
 
-    cell.addEventListener('mouseover', () => {
+    e.addEventListener('mouseover', () => {
         if (mouseDown) {
-            cell.classList.add('colored-cell');
+            e.classList.add('colored-cell');
         }
     });
 }
 
+makeGrid(16, 16); //default;
 
+var slider = document.getElementById('grid-size-selector');
+var output = document.getElementById('output');
+let size = '16x16';
+output.innerText = size;
 
+slider.oninput = function() {
+    size = this.value;
+    output.innerText = `${size}x${size}`;
+    console.log(size)
+    makeGrid(size);
+}
