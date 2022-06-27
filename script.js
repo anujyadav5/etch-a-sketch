18425b5da52b38dc9955f9c933c3e67868ee6709
anujy <anuj.y@hotmail.com>
2022-06-27 17:59:39 -0400

    added functional clear and erase button; updated event listeners as well

--- script.js
+++ script.js
@@ -1,41 +1,49 @@
-const gridContainer = document.getElementsByClassName('grid-container')[0];
+const DEFAULT_SIZE = 16;
+const DEFAULT_MODE = 'color';
 
-function clearGrid(element) {
+const gridContainer = document.getElementsByClassName('grid-container')[0];
+const clearButton = document.getElementById('clear');
+const eraserButton = document.getElementById('eraser');
+const shaderButton = document.getElementById('shader');
+
+let currentMode = DEFAULT_MODE;
+let mouseDown = false;
+
+document.addEventListener('mouseup', () => {mouseDown=false});
+
+function resetGrid(element) {
     while(element.firstChild) {
         element.removeChild(element.firstChild);
     }
 }
 
 function makeGrid(size) { //generate size x size grid;
-    clearGrid(gridContainer);
+    resetGrid(gridContainer);
     gridContainer.style.setProperty('--grid-rows', size);
     gridContainer.style.setProperty('--grid-columns', size);
     for (let n=0; n<(size**2); n++) {
         let cell = document.createElement('div');
         cell.className = 'grid-cell';
-        sketchableCell(cell);
+        cell.addEventListener('mousedown', toggleCell);
+        cell.addEventListener('mouseover', toggleCell);
         gridContainer.appendChild(cell);
     }
 }
 
-let mouseDown = false;
-document.addEventListener('mouseup', () => {mouseDown=false});
 
-function sketchableCell(e) {
-
-    e.addEventListener('mousedown', () => {
+function toggleCell(e) {
+    if((e.type == 'mouseover' && mouseDown || e.type == 'mousedown')) {
         mouseDown=true;
-        e.classList.add('colored-cell');
-    });
-
-    e.addEventListener('mouseover', () => {
-        if (mouseDown) {
-            e.classList.add('colored-cell');
+        if (currentMode == DEFAULT_MODE) {
+            e.target.classList.add('colored-cell');
+        } else if (currentMode == 'erase') {
+            e.target.classList.remove('colored-cell');
+        } else if (currentMode == 'shade') {
+            
         }
-    });
+    }
 }
 
-makeGrid(16, 16); //default;
 
 var slider = document.getElementById('grid-size-selector');
 var output = document.getElementById('output');
@@ -45,6 +53,26 @@ output.innerText = size;
 slider.oninput = function() {
     size = this.value;
     output.innerText = `${size}x${size}`;
-    console.log(size)
     makeGrid(size);
 }
+
+
+clearButton.onclick = () => {
+    let cells = Array.from(document.getElementsByClassName('grid-cell'));
+    for (let i=0; i<cells.length; i++) {
+        cells[i].classList.remove('colored-cell');
+    }
+}
+
+eraserButton.onclick = () => {
+    (currentMode == 'erase')? (currentMode = 'color'): (currentMode = 'erase');
+}
+
+
+
+
+
+
+window.onload = () => {
+    makeGrid(DEFAULT_SIZE);
+} 
